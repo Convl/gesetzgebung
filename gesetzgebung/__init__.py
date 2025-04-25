1e2a4a1b1dfdc3f6f1ec916886f2f990f50134dc
Constantin van Lijnden <cvanlijnden@gmail.com>
2025-04-25 17:12:16 +0200

    make daily_update import conditional on IS_WEBAPP env var

--- gesetzgebung/__init__.py
+++ gesetzgebung/__init__.py
@@ -1,6 +1,7 @@
 from gesetzgebung.config import app, db, es
+import os
 
-
-# from gesetzgebung import routes
-from gesetzgebung import daily_update
+# TODO: Separate daily_update out altogether. For now, this is a hack to make sure the webapp on heroku does not import it, but GitHub Actions does. Mental note: There also is a .slugignore file with daily_update in it.
+if os.environ.get("IS_WEBAPP") not in {True, "true", "True"}:
+    from gesetzgebung import daily_update
 
